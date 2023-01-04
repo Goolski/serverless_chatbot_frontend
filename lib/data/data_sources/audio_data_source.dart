9eb482b3b75f6f79101e5023ef1e20fabed5c63a
GWEiRDNA <andr0lpoczta@gmail.com>
2023-01-04 09:53:45 +0100

    Audio working from stream

--- lib/data/data_sources/audio_data_source.dart
+++ lib/data/data_sources/audio_data_source.dart
@@ -1,42 +1,20 @@
-import 'dart:convert';
-import 'dart:io';
-import 'dart:typed_data';
-
 import 'package:injectable/injectable.dart';
 import 'package:just_audio/just_audio.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:uuid/uuid.dart';
 
 @injectable
 class AudioDataSource {
-  AudioDataSource() {
-    getTemporaryDirectory().then((value) {
-      _directory = value;
-      _file = File('${_directory.path}/$filename.mp3');
-    });
-  }
-
   final AudioPlayer _audioPlayer = AudioPlayer();
-  final filename = const Uuid().v4().substring(0, 20);
-  late final Directory _directory;
-  late final File _file;
 
   Future<void> playAudio({required List<int> audioBytes}) async {
-    await saveAudio(audioBytes);
-    await _audioPlayer.setUrl('file://${_directory.path}/$filename.mp3');
-    await _audioPlayer.play();
-    return;
+    final intAudio = List<int>.from(audioBytes);
+    final source = MpegSource(intAudio);
+    await _audioPlayer.setAudioSource(source);
+    _audioPlayer.play();
   }
 
   Future<void> stopAudio() async {
     await _audioPlayer.stop();
   }
-
-  Future<void> saveAudio(List<int> audio) async {
-    await _file.writeAsBytes(audio);
-  }
-
-  void deleteAudio() {}
 }
 
 class MpegSource extends StreamAudioSource {
